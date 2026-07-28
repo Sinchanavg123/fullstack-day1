56a1ed55142c4d57ee287247b38e4d7781f4f06f
sinchana <sinchanavghantimath@gmail.com>
2026-07-28 15:42:18 +0530

    Day 3: Professional Home and Blog pages

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the public folder
+// Serve all static files from public
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
